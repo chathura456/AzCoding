83292aeade2ad00077965df2eb36b27d2db0e343
chathura456 <chathuracde@gmail.com>
2021-06-17 03:06:17 +0530

    register php setup

---
+++ register.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SIGN UP</title>
+	<link rel="stylesheet" type="text/css" href="loginregister.css">
+</head>
+<body>
+
+     <form action="Register-check.php" method="post">
+          <center>
+          <div id="azlogo"></div>
+     	<h2>Register</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+
+          <?php if (isset($_GET['success'])) { ?>
+               <p class="success"><?php echo $_GET['success']; ?></p>
+          <?php } ?>
+
+        
+          <?php if (isset($_GET['name'])) { ?>
+               <input type="text" 
+                      name="name" 
+                      placeholder="Full Name"
+                      value="<?php echo $_GET['name']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="name" 
+                      placeholder="Full Name"><br>
+          <?php }?>
+
+         
+          <?php if (isset($_GET['uname'])) { ?>
+               <input type="email" 
+                      name="uname" 
+                      placeholder="Email"
+                      value="<?php echo $_GET['uname']; ?>"><br>
+          <?php }else{ ?>
+               <input type="email" 
+                      name="uname" 
+                      placeholder="Email"><br>
+          <?php }?>
+
+     	<input type="password" 
+                 name="password" 
+                 placeholder="Password"><br>
+
+          <input type="password" 
+                 name="re_password" 
+                 placeholder="Re_Password"><br>
+
+     	<button type="submit">Register</button>
+         <p>Already have an account?<a href="login.html" class="ca">Login Now</a></p> 
+          </center>
+     </form>
+</body>
+</html>
