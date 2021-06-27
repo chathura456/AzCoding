b762c42ddc15db9e4a5a9e5f5a98caacb205a786
LamaliJeewanthi <lamalijeewanthi@gmail.com>
2021-06-27 22:41:49 +0530

    edit 2

---
+++ Category-Branch/js/Cprogamming.js
@@ -0,0 +1,92 @@
+function myHTMLFunction() {  // function for sign in 
+    
+    var home = (confirm ("Do you want to CODE in HTML"));
+
+    if(home){
+
+        window.open("compiler.html"); // press ok automaticly open complier page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+ 
+}
+
+
+function myCSSFunction() {  // function for sign in 
+    
+    var home = (confirm ("Do you want to CODE in CSS"));
+
+    if(home){
+
+        window.open("compiler.html"); // press ok automaticly open complier page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+ 
+}
+
+function myCCCFunction() {  // function for sign in 
+    
+    var home = (confirm ("Do you want to CODE in C++"));
+
+    if(home){
+
+        window.open("compiler.html"); // press ok automaticly open complier page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+ 
+}
+
+function myJAVAFunction() {  // function for sign in 
+    
+    var home = (confirm ("Do you want to CODE in JAVA"));
+
+    if(home){
+
+        window.open("compiler.html"); // press ok automaticly open complier page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+ 
+}
+
+
+
+function myCFunction() {  // function for sign in 
+    
+    var home = (confirm ("Do you want to CODE in C"));
+
+    if(home){
+
+        window.open("compiler.html"); // press ok automaticly open complier page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+ 
+}
