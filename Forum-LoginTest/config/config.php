c2e0b6fa145ab530ef57fa1f121466b86fd5489b
chathura456 <chathuracde@gmail.com>
2021-07-05 22:14:15 +0530

    database edit in forums

--- Forum-LoginTest/config/config.php
+++ Forum-LoginTest/config/config.php
@@ -2,10 +2,10 @@
 //DB Params
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "121212aa");
+define("DB_PASS", "");
 define("DB_NAME", "talkingspace");
 
-define("SITE_TITLE", "Welcome To TalkingSpace!");
+define("SITE_TITLE", "Welcome To AZ Coding!");
 
 //Paths
 define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/talkingspace/');
