53f9691467c1e4055232aba8567ad2413760e6f9
chathura456 <chathuracde@gmail.com>
2021-07-15 15:22:28 +0530

    fix all category filter errors

--- Master-Branch/Forum-NewTest/comment-reply-system/post_details.php
+++ Master-Branch/Forum-NewTest/comment-reply-system/post_details.php
@@ -114,13 +114,59 @@ if(isset($_GET['submit'])){
         
     }
 
-}else{
-	echo "Chosen colour : ";
-	
-}
-?>
+}else{?> 
+	 echo "Chosen colour : ";
+	 <h2><span id="comments_count"><?php echo count($comments) ?></span> Comment(s)</h2>
+	 <div id="comments-wrapper">
+			<?php if (isset($comments)): ?>
+				<!-- Display comments -->
+				<?php foreach ($comments as $comment): ?>
+				<!-- comment -->
+				<div class="comment clearfix">
+					<img src="profile.png" alt="" class="profile_pic">
+					<div class="comment-details">
+						<span class="comment-name"><?php echo getUsernameById($comment['user_id']) ?></span>
+						<span class="comment-date"><?php echo date("F j, Y ", strtotime($comment["created_at"])); ?></span>
+						<p><?php echo $comment['body']; ?></p>
+						<a class="reply-btn" href="#" data-id="<?php echo $comment['id']; ?>">reply</a>
+					</div>
+					<!-- reply form -->
+					<form action="post_details.php" class="reply_form clearfix" id="comment_reply_form_<?php echo $comment['id'] ?>" data-id="<?php echo $comment['id']; ?>">
+						<textarea class="form-control" name="reply_text" id="reply_text" cols="30" rows="2"></textarea>
+						<button class="btn btn-primary btn-xs pull-right submit-reply">Submit reply</button>
+					</form>
+
+					<!-- GET ALL REPLIES -->
+					<?php $replies = getRepliesByCommentId($comment['id']) ?>
+					<div class="replies_wrapper_<?php echo $comment['id']; ?>">
+						<?php if (isset($replies)): ?>
+							<?php foreach ($replies as $reply): ?>
+								<!-- reply -->
+								<div class="comment reply clearfix">
+									<img src="profile.png" alt="" class="profile_pic">
+									<div class="comment-details">
+										<span class="comment-name"><?php echo getUsernameById($reply['user_id']) ?></span>
+										<span class="comment-date"><?php echo date("F j, Y ", strtotime($reply["created_at"])); ?></span>
+										<p><?php echo $reply['body']; ?></p>
+										<a class="reply-btn" href="#">reply</a>
+									</div>
+								</div>
+							<?php endforeach ?>
+						<?php endif ?>
+					</div>
+				</div>
+					<!-- // comment -->
+				<?php endforeach ?>
+			<?php else: ?>
+				<h2>Be the first to comment on this post</h2>
+			<?php endif ?>
+			</div><!-- comments wrapper -->
+		</div><!-- // all comments -->
+<?php	
+}?>
+
 			<!-- Display total number of comments on this post  -->
-			<h2><span id="comments_count"><?php echo count($comments) ?></span> Comment(s)</h2>
+			
 			<hr>
 			<!-- comments wrapper -->
 			<div id="comments-wrapper">
