525d0063ec88fd871d6993d43139468731252179
lamalijeewanthi <lamalijeewanthi@gmail.com>
2021-07-16 19:51:10 +0530

    commit 24

--- Category-Branch/js/Cprogamming.js
+++ Category-Branch/js/Cprogamming.js
@@ -90,3 +90,111 @@ function myCFunction() {  // function for sign in
 
  
 }
+
+function myAllFunction() {
+
+    var all = (confirm ("Do you want go to Category"));
+
+    if(all){
+
+        window.open("Cprogramming.html"); // press ok automaticly open complier page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+}
+
+
+function myHtmlLessonFunction() {
+
+    var all = (confirm ("Do you want Know More About HTML"));
+
+    if(all){
+
+        window.open("htmllesson.html"); // press ok automaticly open HTML Lesson page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+}
+
+function myCssLessonFunction() {
+
+    var all = (confirm ("Do you want Know More About CSS"));
+
+    if(all){
+
+        window.open("csslesson.html"); // press ok automaticly open HTML Lesson page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+}
+
+
+
+function myCccLessonFunction() {
+
+    var all = (confirm ("Do you want Know More About C++"));
+
+    if(all){
+
+        window.open("ccc.html"); // press ok automaticly open HTML Lesson page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+}
+
+
+function myJavaLessonFunction() {
+
+    var all = (confirm ("Do you want Know More About JAVA"));
+
+    if(all){
+
+        window.open("ccc.html"); // press ok automaticly open HTML Lesson page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+}
+
+
+
+function myCLessonFunction() {
+
+    var all = (confirm ("Do you want Know More About C Language"));
+
+    if(all){
+
+        window.open("clesson.html"); // press ok automaticly open HTML Lesson page
+    }
+
+    else {
+
+        window.alert("You Press Cancel...!"); // press cancel stay in  category page
+
+    }
+
+}
