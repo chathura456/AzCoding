524f3e49f4a46961a0ed2a5c2db4074c19c40015
chathura456 <chathuracde@gmail.com>
2021-08-19 13:47:09 +0530

    new compiler navbar change

--- New-Compiler/Compiler.php
+++ New-Compiler/Compiler.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Compiler</title>
 	<link rel="stylesheet" type="text/css" href="https://codemirror.net/lib/codemirror.css">
 	<script type="text/javascript" src="https://codemirror.net/lib/codemirror.js"></script>
 	<script  type="text/javascript" src="https://codemirror.net/mode/xml/xml.js"></script>
 	<script  type="text/javascript" src="https://codemirror.net/mode/css/css.js"></script>
 	<script  type="text/javascript" src="https://codemirror.net/mode/javascript/javascript.js"></script>
+	<script  type="text/javascript" src="https://codemirror.net/mode/htmlmixed/htmlmixed.js"></script>
 
 	<link rel="stylesheet" type="text/css" href="Compiler.css">
 </head>
 <body>
 
 	<div class="header">
-	<a href="index.php"><img src="logo2.png" style="width: 200px; float:left"></a> 
+	<a href="/AzCoding-Website/AzCoding/index.php"><img src="logo2.png" style="width: 200px; float:left"></a> 
 		<button id="run-btn">Run</button>
 	</div>
 	<div class="editor">
@@ -21,6 +22,7 @@
 			<div class="html-code"><h1>HTML</h1></div>
 			<div class="css-code"><h1>CSS</h1></div>
 			<div class="js-code"><h1>JavaScript</h1></div>
+			
 		</div>
 		<div class="preview">
 			<iframe id="preview-window"></iframe>
--- New-Compiler/code.js
+++ New-Compiler/code.js
@@ -1,16 +1,16 @@
 const htmlEditor = CodeMirror(document.querySelector(".editor .code .html-code"),{
 	lineNumbers:true,
-	tabSize:4,
-	mode:"xml"
+	tabSize:3,
+	mode:"html-mixed"
 });
 const cssEditor = CodeMirror(document.querySelector(".editor .code .css-code"),{
 	lineNumbers:true,
-	tabSize:4,
+	tabSize:3,
 	mode:"css"
 });
 const jsEditor = CodeMirror(document.querySelector(".editor .code .js-code"),{
 	lineNumbers:true,
-	tabSize:4,
+	tabSize:3,
 	mode:"javascript"
 });
 
@@ -18,8 +18,9 @@ document.querySelector("#run-btn").addEventListener("click",function(){
 	let htmlCode = htmlEditor.getValue();
 	let cssCode = "<style>" + cssEditor.getValue() + "</style>";
 	let jsCode = "<scri" + "pt>" + jsEditor.getValue() + "</scri" + "pt>";
+	let clikeCode = clikeEditor.getValue();
 	let previewWindow = document.querySelector("#preview-window").contentWindow.document;
 	previewWindow.open();
-	previewWindow.write(htmlCode+cssCode+jsCode);
+	previewWindow.write(htmlCode+cssCode+jsCode+clikeCode);
 	previewWindow.close();
 });
